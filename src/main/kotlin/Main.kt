5cd26eb3860fb2d435c42bced1ac0bf9151d8b37
Rodrigo Brito do Nascimento <rodrito90@gmail.com>
2024-03-12 21:15:57 -0300

    First request

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
-fun main(args: Array<String>){
-    println("Hello World!")
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+    val response = client
+        .send(request, HttpResponse.BodyHandlers.ofString())
+
+    val json = response.body()
+
+    println(json)
 }
